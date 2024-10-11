9b2706e80f911c90e4101d77face57fac830c895
Marcus Vinicius <marcusviny63@gmail.com>
2024-10-11 17:25:08 +0000

    fix pmx collisions

--- BPP/GGA.py
+++ BPP/GGA.py
@@ -546,8 +546,7 @@ class GGA:
 
         Args:
             child_elements (list): Os elementos do filho gerado após a troca.
-            parent1_elements (list): Elementos do primeiro pai.
-            parent2_elements (list): Elementos do segundo pai.
+            mapping (dict): Mapeamento entre os itens trocados nos pais.
             point1 (int): Primeiro ponto de cruzamento.
             point2 (int): Segundo ponto de cruzamento.
         """
@@ -559,35 +558,40 @@ class GGA:
             while child_elements[i] in swapped_elements:
                 child_elements[i] = mapping[child_elements[i]]
 
-    # Função para otimização dos hiperparametross
-    def call_optuna(self):
-        def objective(trial):
-            # Hiperparâmetros a serem otimizados
-            hyperparameters = {
-                'num_generations': trial.suggest_int('num_generations', 50, 200),
-                'population_size': trial.suggest_int('population_size', 10, 100),
-                'mutation_rate': trial.suggest_float('mutation_rate', 0.01, 0.5),
-                'elite_rating': trial.suggest_float('elite_rating', 0.01, 0.5),
-                'tabu_max_iterations': trial.suggest_int('tabu_max_iterations', 10, 100),
-                'tabu_tenure': trial.suggest_int('tabu_tenure', 1, 20),
-                'tabu_max_neighbors': trial.suggest_int('tabu_max_neighbors', 5, 50),
-                'weights': [random.randint(1, 100) for _ in range(50)],
-                'bin_capacity': 150
-            }
+# Função para otimização dos hiperparametross
+# def call_optuna(self):
 
-            # Inicializar e executar o GGA
-            gga = GGA(hyperparameters)
 
-            best_solution = gga.run()
-            best_fitness = gga.fitness(best_solution)
+"""
+def objective(trial):
+    # Hiperparâmetros a serem otimizados
+    hyperparameters = {
+        'num_generations': trial.suggest_int('num_generations', 50, 200),
+        'population_size': trial.suggest_int('population_size', 10, 100),
+        'mutation_rate': trial.suggest_float('mutation_rate', 0.01, 0.5),
+        'elite_rating': trial.suggest_float('elite_rating', 0.01, 0.5),
+        'tabu_max_iterations': trial.suggest_int('tabu_max_iterations', 10, 100),
+        'tabu_tenure': trial.suggest_int('tabu_tenure', 1, 20),
+        'tabu_max_neighbors': trial.suggest_int('tabu_max_neighbors', 5, 50),
+        'weights': [random.randint(1, 100) for _ in range(50)],
+        'bin_capacity': 150
+    }
 
-            # O Optuna minimiza por padrão, então fitness negativo pode ser necessário se maior for melhor
-            return best_fitness
+    # Inicializar e executar o GGA
+    gga = GGA(hyperparameters)
 
-        # Alterar para "maximize" se necessário
-        study = optuna.create_study(direction="minimize")
-        study.optimize(objective, n_trials=20)
+    best_solution = gga.run()
+    best_fitness = gga.fitness(best_solution)
 
-        # Exibir os melhores hiperparâmetros
-        print("Melhores hiperparâmetros encontrados:", study.best_params)
-        return study.best_params
+    # O Optuna minimiza por padrão, então fitness negativo pode ser necessário se maior for melhor
+    return best_fitness
+
+
+# Alterar para "maximize" se necessário
+study = optuna.create_study(direction="minimize")
+study.optimize(objective, n_trials=20)
+
+# Exibir os melhores hiperparâmetros
+print("Melhores hiperparâmetros encontrados:", study.best_params)
+# return study.best_params
+"""
